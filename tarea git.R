d2b0f083e56f1846948712bad09fee57165c9180
LuTapia <l.tapia@socialxchange.org>
2021-05-22 11:45:37 -0400

    librerias Python

--- tarea git.R
+++ tarea git.R
@@ -16,3 +16,32 @@ plot(cars, xlim = c(10, 15))
 
 remotes::install_github("rstudio/renv")
 renv::init()
+sessionInfo()
+
+#prueba de commit
+
+usethis::edit_r_environ()
+install.packages("reticulate")
+
+library(reticulate)
+##inicio
+py_install("pandas")
+# create a new environment 
+conda_create("r-reticulate")
+# install SciPy
+conda_install("r-reticulate", "scipy")
+# indicate that we want to use a specific condaenv
+use_condaenv("r-reticulate")
+# import SciPy (it will be automatically discovered in "r-reticulate")
+scipy <- import("scipy")
+scipy$amin(c(1,3,5,7))
+##fin
+
+#ejemplos uso python y entornos
+library(reticulate)
+use_python("/usr/local/bin/python")
+use_virtualenv("~/myenv")
+use_condaenv("myenv")
+use_condaenv(condaenv = "r-nlp", conda = "/opt/anaconda3/bin/conda") #version alternativa específica
+#info especifica de la versión de Python en uso
+py_config()
